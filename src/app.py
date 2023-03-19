63b5de69a1c7fa7f607c2dd42f2cc38a2f16338d
kwiecs01 <szymon.kwiecinski@stepstone.com>
2023-03-19 12:31:58 +0100

    remove commented code

--- src/app.py
+++ src/app.py
@@ -14,38 +14,6 @@ def create_app():
     slug_to_project = {project['slug']: project for project in projects}
     print(slug_to_project)
 
-
-#
-# projects = [
-#     {
-#         "name": "Habit tracking app with Python and MongoDB",
-#         "text": "lapis habit",
-#         "thumb": "img/habit-tracking.png",
-#         "hero": "img/habit-tracking-hero.png",
-#         "categories": ["python", "web"],
-#         "slug": "habit-tracking",
-#         "prod": "https://udemy.com",
-#     },
-#     {
-#         "name": "Personal finance tracking app with React",
-#         "text": "lapis personal",
-#         "thumb": "img/personal-finance.png",
-#         "hero": "img/personal-finance.png",
-#         "categories": ["react", "javascript"],
-#         "slug": "personal-finance",
-#     },
-#     {
-#         "name": "REST API Documentation with Postman and Swagger",
-#         "text": "lapis rest api",
-#         "thumb": "img/rest-api-docs.png",
-#         "hero": "img/rest-api-docs.png",
-#         "categories": ["writing"],
-#         "slug": "api-docs",
-#     },
-# ]
-#
-# slug_to_projectect = {project['slug']: project for project in projects}
-
     @app.route("/")
     def home():
         return render_template("home.html", projects=projects)
